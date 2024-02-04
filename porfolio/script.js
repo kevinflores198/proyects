dc8e34a5e7c76dca51dcd4e2063860ef15e3de10
kevin flores <kevinfloresg1998@gmail.com>
2024-02-03 21:22:32 -0400

    first

---
+++ porfolio/script.js
@@ -0,0 +1,10 @@
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header')
+
+    if (window.scrolly > 0) {
+        header.classList.add('scrolled')
+    } else {
+        header.classList.remove('scrolled')
+    }
+})
